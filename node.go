e52d94c3453978c3d38c8920f779fddbeedc463a
Sumit Asok <sumitasok@gmail.com>
2015-01-28 16:48:15 +0530

    theTree: Append, if Key doesn't exists.

--- node.go
+++ node.go
@@ -1,7 +1,12 @@
 package theTree
 
 import (
+	"errors"
 	"reflect"
+)
+
+const (
+	err_key_already_exists = "Key already exists, use Update"
 )
 
 type Node struct {
@@ -26,4 +31,25 @@ func (n *Node) Set(value interface{}) *Node {
 	n.Value = value
 	n.DataType = reflect.TypeOf(value).String()
 	return n
+}
+
+func (n *Node) Append(key string) (*Node, error) {
+	child := Node{n.Engine, Core{Key: key, Parent: n.Parent}}
+	exists := false
+	for _, c := range n.Children {
+		if c.Key == key {
+			exists = true
+		}
+	}
+
+	if exists {
+		child = Node{}
+		return &child, errors.New(err_key_already_exists)
+	}
+
+	child.Key = key
+	child.Parent = n
+	n.Children = append(n.Children, &child)
+
+	return &child, nil
 }
--- node_test.go
+++ node_test.go
@@ -14,7 +14,6 @@ func TestNodeInit(t *testing.T) {
 
 	node := Init(engine, key)
 
-	assert.True(true)
 	assert.NotNil(node)
 }
 
@@ -27,7 +26,28 @@ func TestSet(t *testing.T) {
 	node := Init(engine, key)
 	node.Set("value")
 
-	assert.True(true)
 	assert.Equal("value", node.Value)
 	assert.Equal("string", node.DataType)
+}
+
+func TestAppend(t *testing.T) {
+	assert := assert.New(t)
+
+	engine := Normal{}
+	key := "root"
+
+	node := Init(engine, key)
+	node.Set("value")
+
+	newNode, err := node.Append("key")
+	assert.Equal("key", node.Children[0].Key)
+	assert.Equal("key", newNode.Key)
+	assert.NoError(err)
+
+	newNode, err = node.Append("key")
+	assert.Equal("key", node.Children[0].Key)
+	assert.NotEqual("key", newNode.Key)
+	assert.Error(err)
+	assert.Equal(err_key_already_exists, err.Error())
+
 }
