4267a5719a593355fb948653a6a2f01433c07b5c
Sumit Asok <sumitasok@gmail.com>
2015-01-29 18:13:59 +0530

    json: only colon

--- json.go
+++ json.go
@@ -15,3 +15,49 @@ func stripOuterCurls(str string) string {
 	finalIndex := strings.LastIndex(str, "}")
 	return str[initialIndex+1 : finalIndex]
 }
+
+func stripOuter(str string, sep string) string {
+	initialIndex := strings.Index(str, sep)
+	finalIndex := strings.LastIndex(str, sep)
+	return str[initialIndex+1 : finalIndex]
+}
+
+func fetchInnerCurls(str string) Element {
+	e := Element{Content: str}
+	str = stripOuterCurls(str)
+	e.Content = str
+
+	for _, split := range strings.Split(e.Content, ",") {
+		t := Element{Content: split}
+		e.Next = append(e.Next, &t)
+	}
+
+	// for _, element := range e.Next {
+
+	// }
+	return e
+}
+
+type Element struct {
+	Content string
+	Next    []*Element
+	Key     string
+	Value   string
+}
+
+func (e *Element) OnlyColon() {
+	if strings.Contains(e.Content, ",") == false {
+		if strings.Contains(e.Content, "{") == false {
+			if strings.Contains(e.Content, "}") == false {
+				keyValue := strings.Split(e.Content, ":")
+				if len(keyValue) == 2 {
+					e.Key = stripOuter(keyValue[0], "\"")
+					e.Value = stripOuter(keyValue[1], "\"")
+				}
+			}
+		}
+	}
+}
+
+// node is a data struct thure which allows
+// nested key value pair data to be traversed in many ways
--- json_test.go
+++ json_test.go
@@ -29,3 +29,27 @@ func TestStripOuterCurls(t *testing.T) {
 
 	assert.Equal("\"holder\": \"holds\"", result)
 }
+
+func TestFetchInnerCurls(t *testing.T) {
+	assert := assert.New(t)
+
+	str := string([]byte(`{"holder": "holds"}`))
+	result := fetchInnerCurls(str)
+	assert.Equal(string([]byte(`"holder": "holds"`)), result.Content)
+
+	str = string([]byte(`{"holder": "holds","hash": {"inner": "peace"}}`))
+	result = fetchInnerCurls(str)
+	assert.Equal(string([]byte(`"holder": "holds"`)), result.Next[0].Content)
+	// assert.Equal(string([]byte(`"inner": "peace"`)), result.Next[1].Next[0].Content)
+
+}
+
+func TestOnlyColon(t *testing.T) {
+	assert := assert.New(t)
+
+	str := string([]byte(`"holder": "holds"`))
+	e := Element{Content: str}
+	e.OnlyColon()
+	assert.Equal("holder", e.Key)
+	assert.Equal("holds", e.Value)
+}
