c30f08beee05f41cc58cd3733b6b44c97f120517
Sumit Asok <sumitasok@gmail.com>
2015-02-07 02:21:05 +0530

    best

--- full_test.go
+++ full_test.go
@@ -10,27 +10,56 @@ package theTree
 // then parse and create CHild Nodes and assign their values
 
 import (
-	"fmt"
+	// "bytes"
+	// "fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestStringParse(t *testing.T) {
+// func TestStringParse(t *testing.T) {
+// 	assert := assert.New(t)
+
+// 	byteArr := []byte(`{key: "value \"", "key2" : []}`)
+
+// 	engine := Normal{}
+// 	key := "root"
+
+// 	node := Init(engine, key)
+
+// 	err := SetNodeValue(node, byteArr)
+// 	expectedByteArr := []byte(` "key2" : []`)
+// 	assert.NoError(err)
+
+// 	actualByteArr, _ := node.Value.([]uint8)
+// 	assert.Equal(string(expectedByteArr), string(actualByteArr))
+
+// 	fmt.Println("-----------------------------------")
+// }
+
+func TestParseNode(t *testing.T) {
 	assert := assert.New(t)
 
-	byteArr := []byte(`{key: "value \"", "key2" : []}`)
+	byteArr := []byte(`key: "value \"", "key2" : []`)
+
+	// for i, r := range bytes.Runes(byteArr) {
+	// 	fmt.Println(i, " - ", r, " - ", string(r))
+	// }
 
 	engine := Normal{}
 	key := "root"
 
 	node := Init(engine, key)
 
-	err := SetNodeValue(node, byteArr)
-	expectedByteArr := []byte(`key: "value \"", "key2" : []`)
+	node.Set(byteArr)
+
+	parseNode(node, byteArr)
+
+	newNode, err := node.Child("key")
+	assert.Equal("key", newNode.Key)
 	assert.NoError(err)
 
-	actualByteArr, _ := node.Value.([]uint8)
-	assert.Equal(string(expectedByteArr), string(actualByteArr))
+	newNodeValue, err1 := newNode.Value.([]uint8)
+	assert.Equal("key", string(newNodeValue))
+	assert.False(err1)
 
-	fmt.Println("-----------------------------------")
 }
--- string.go
+++ string.go
@@ -3,7 +3,7 @@ package theTree
 import (
 	"bytes"
 	"errors"
-	// "fmt"
+	"fmt"
 )
 
 const (
@@ -16,10 +16,13 @@ const (
 	R_OPEN_SQUARE   = 91
 	R_CLOSED_SQUARE = 93
 	R_TAB           = 9
+	R_COMMA         = 44
 )
 
 func SetNodeValue(node *Node, byteArr []byte) error {
 	byteArr = bytePluckByteRecursively(byteArr, R_SPACE, R_SPACE)
+
+	node.Set(byteArr)
 
 	if byteIs(byteArr, R_OPEN_CURL) {
 		if byteArr, err := byteStripOuterCurls(byteArr); err != nil {
@@ -29,7 +32,64 @@ func SetNodeValue(node *Node, byteArr []byte) error {
 		}
 	}
 
+	if byteIs(byteArr, R_OPEN_SQUARE) {
+		return errors.New(("arrays undefined"))
+	}
+
+	value := byteArr
+
+	parseNode(node, value)
+
 	return nil
+}
+
+func parseNode(node *Node, value []byte) {
+	byteArr, _ := node.Value.([]uint8)
+	// go through each , seperated key-value pair
+	// and create a child
+
+	str, _ := node.Value.([]uint8)
+	fmt.Println("parseNode 1", node.Key, string(str))
+
+	byteArrOfArr, err := byteSplitKeyValue(byteArr, []byte(`:`), 2)
+	if err == nil {
+		fmt.Println("err nil in byteSplitKeyValue", string(byteArrOfArr[0]))
+		newNode, errAppend := node.Append(string(byteArrOfArr[0]))
+		if errAppend == nil {
+			fmt.Println("err nil in Append", string(byteArrOfArr[1]))
+			// split on next, and then set value,
+			// and send next parse with remaining value
+			baa, errGetValue := GetValue(byteArrOfArr[1])
+			if errGetValue == nil {
+				fmt.Println("err nil in GetValue")
+				SetNodeValue(newNode, baa[0])
+				fmt.Println("parseNode 3", newNode.Key, string(baa[0]))
+
+				SetNodeValue(node, baa[1])
+				fmt.Println("parseNode 4", node.Key, string(baa[1]))
+
+				parseNode(node)
+			}
+			fmt.Println("parseNode 2", newNode.Key, string(byteArrOfArr[1]))
+			SetNodeValue(newNode, byteArrOfArr[1])
+		}
+	}
+}
+
+func GetValue(byteArr []byte) ([][]byte, error) {
+	curl_count := 0
+	sqaure_count := 0
+	quotes := 0
+	for i, b := range byteArr {
+		if b == R_COMMA && curl_count == 0 && sqaure_count == 0 && quotes == 0 {
+			return [][]byte{byteArr[:i], byteArr[i+1:]}, nil
+		}
+		// if b == R_OPEN_CURL {
+		// 	curl_count = curl_count + 1
+		// }
+		// fmt.Println(i, b)
+	}
+	return [][]byte{}, errors.New("error")
 }
 
 func byteSplitKeyValue(byteArr, sep []byte, n int) ([][]byte, error) {
