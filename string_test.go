29c6722bc3bba3d894f8711fe4086533d0db2118
Sumit Asok <sumitasok@gmail.com>
2015-02-07 00:39:26 +0530

    json decode tests in a file

--- string_test.go
+++ string_test.go
@@ -1,40 +1,11 @@
 package theTree
 
-// A double quote ends with another double quotes
-//  - escape \"
-// open { ends in } where any of } occurring in " or '
-// is escaped
-
-// Node{}
-// set value as the string
-// then parse and create CHild Nodes and assign their values
-
 import (
 	// "bytes"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
-
-func TestStringParse(t *testing.T) {
-	assert := assert.New(t)
-
-	byteArr := []byte(`{key: "value \"", "key2" : []}`)
-
-	engine := Normal{}
-	key := "root"
-
-	node := Init(engine, key)
-
-	err := SetNodeValue(node, byteArr)
-	expectedByteArr := []byte(`key: "value \"", "key2" : []`)
-	assert.NoError(err)
-
-	actualByteArr, _ := node.Value.([]uint8)
-	assert.Equal(string(expectedByteArr), string(actualByteArr))
-
-	fmt.Println("-----------------------------------")
-}
 
 func TestByteStripOuterCurls(t *testing.T) {
 	assert := assert.New(t)
