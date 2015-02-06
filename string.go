55587d4aa3059626547b80e25301f4a1e559b7c7
Sumit Asok <sumitasok@gmail.com>
2015-02-06 23:08:48 +0530

    byte operation strip space and check if hash

---
+++ string.go
@@ -0,0 +1,63 @@
+package theTree
+
+import (
+// "bytes"
+)
+
+const (
+	R_OPEN_CURL     = 123
+	R_CLOSED_CURLED = 125
+	R_COLON         = 58
+	R_SPACE         = 32
+	R_DOUBLE_QOUTE  = 34
+	R_SLASH_N       = 10
+	R_OPEN_SQUARE   = 91
+	R_CLOSED_SQUARE = 93
+	R_TAB           = 9
+)
+
+func SetNodeValue(node *Node, byteArr []byte) {
+	if node.parent == nil {
+		// stripOuterCurls(string(byteArr))
+		byteArr = byteRemoveSpace(byteArr)
+	}
+}
+
+// func byteStripOuterCurls(byteArr []byte) []byte {
+
+// }
+
+func byteRemoveSpace(byteArr []byte) []byte {
+	if len(byteArr) == 0 {
+		return byteArr
+	} else {
+		if byteArr[0] != R_SPACE {
+			return byteArr
+		} else {
+			return byteRemoveSpace(byteArr[1:])
+		}
+	}
+}
+
+func byteRemoveByte(byteArr []byte, byteChar byte) []byte {
+	if len(byteArr) == 0 {
+		return byteArr
+	} else {
+		if byteArr[0] != byteChar {
+			return byteArr
+		} else {
+			return byteRemoveSpace(byteArr[1:])
+		}
+	}
+}
+
+func byteIsHash(byteArr []byte) bool {
+	byteArr = byteRemoveByte(byteArr, R_SPACE)
+	if len(byteArr) == 0 {
+		return false
+	} else if byteArr[0] == R_OPEN_CURL {
+		return true
+	} else {
+		return false
+	}
+}
---
+++ string_test.go
@@ -0,0 +1,83 @@
+package theTree
+
+// A double quote ends with another double quotes
+//  - escape \"
+// open { ends in } where any of } occurring in " or '
+// is escaped
+
+// Node{}
+// set value as the string
+// then parse and create CHild Nodes and assign their values
+
+import (
+	// "bytes"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStringParse(t *testing.T) {
+	// assert := assert.New(t)
+
+	// byteArray := []byte(`{key: "value \"", "key2" : []}`)
+
+	// for i, r := range bytes.Runes(byteArray) {
+	// 	fmt.Println(i, " - ", r, " - ", string(r))
+	// }
+
+	// engine := Normal{}
+	// key := "root"
+
+	// node := Init(engine, key)
+
+	// SetNodeValue(node, byteArray)
+
+	fmt.Println("-----------------------------------")
+}
+
+func TestByteRemoveSpace(t *testing.T) {
+	assert := assert.New(t)
+
+	byteArr := []byte(``)
+	assert.Empty(byteArr)
+
+	byteArr = byteRemoveSpace([]byte(` `))
+	assert.Empty(byteArr)
+
+	byteArr = byteRemoveSpace([]byte(`  `))
+	assert.Empty(byteArr)
+
+	byteArr = byteRemoveSpace([]byte(`  {`))
+	assert.Equal([]byte(`{`), byteArr)
+
+}
+
+func TestByteRemoveByte(t *testing.T) {
+	assert := assert.New(t)
+
+	byteArr := []byte(``)
+	assert.Empty(byteArr)
+
+	byteArr = byteRemoveByte([]byte(` `), R_SPACE)
+	assert.Empty(byteArr)
+
+	byteArr = byteRemoveByte([]byte(`  `), R_SPACE)
+	assert.Empty(byteArr)
+
+	byteArr = byteRemoveByte([]byte(`  {`), R_SPACE)
+	assert.Equal([]byte(`{`), byteArr)
+
+}
+
+func TestByteIsHash(t *testing.T) {
+	assert := assert.New(t)
+
+	result := byteIsHash([]byte(` `))
+	assert.False(result)
+
+	result = byteIsHash([]byte(` {`))
+	assert.True(result)
+
+	result = byteIsHash([]byte(` "`))
+	assert.False(result)
+}
