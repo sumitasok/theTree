3bf8a074f044c356e1fd0013b460e28cefb9179f
Sumit Asok <sumitasok@gmail.com>
2015-02-07 12:09:18 +0530

    parse key

--- byteOps/parse.go
+++ byteOps/parse.go
@@ -1,1 +1,44 @@
-package theTree
+package byteOps
+
+import (
+// "fmt"
+)
+
+func prepareKey(byteArr []byte) string {
+
+	byteLen := len(byteArr)
+	revByteArr := reverse(byteArr)
+
+	startIndex, endIndex := 0, 0
+
+	for i, b := range byteArr {
+		if (b > 96 && b < 123) || (b > 64 && b < 91) {
+			startIndex = i
+			break
+		}
+	}
+
+	for i, b := range revByteArr {
+		if (b > 96 && b < 123) || (b > 64 && b < 91) {
+			endIndex = byteLen - i
+			break
+		}
+	}
+
+	return string(byteArr[startIndex:endIndex])
+}
+
+func reverse(b []byte) []byte {
+	r := make([]byte, len(b))
+
+	for i, j := 0, len(b)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = b[j], b[i]
+	}
+
+	return r
+}
+
+//[0a, 1b, 2c , 3d]
+// ri = 1
+// len = 4
+// i = len - r1 - 1
--- byteOps/parse_test.go
+++ byteOps/parse_test.go
@@ -1,1 +1,16 @@
 package byteOps
+
+import (
+	// "fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPrepareKey(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Empty(prepareKey([]byte(``)))
+
+	assert.Equal("abc", prepareKey([]byte(`  abc} `)))
+	assert.Equal("abc", prepareKey([]byte(`  abc`)))
+}
